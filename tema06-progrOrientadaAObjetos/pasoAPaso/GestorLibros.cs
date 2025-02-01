a75e66449decc5fb34630f85766116832d2508e0
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-02-01 13:23:25 +0100

    Gestor de libros, versión 2 (ej.156)

--- tema06-progrOrientadaAObjetos/pasoAPaso/GestorLibros.cs
+++ tema06-progrOrientadaAObjetos/pasoAPaso/GestorLibros.cs
@@ -1,57 +1,70 @@
-/* MARTA (...), retoques menores por Nacho
- * 
-148. A partir de la clase "Libro" (ejercicio 142, en su "solución 
-oficial"), crea un programa que permita tener hasta 25000 libros, y un 
-menú con el que se pueda: Añadir un libro nuevo (opción "1"), Mostrar 
-todos los libros (opción "2"), Buscar libros que contengan un texto 
-determinado (opción "3"), Salir (opción "S"). */
+/* 156. Crea una nueva versión del ejercicio 148 (clase Libro y menú asociado), 
+a partir de la "solución oficial", en la que emplees "this" en el constructor 
+de "Libro" y añadas un constructor que permita indicar título y año, sin autor 
+(que se guardará como "Desconocido"). Este segundo constructor debe apoyarse en 
+el primero. Al añadir datos desde el menú, deberás usar el constructor original 
+si se indica autor, o el nuevo, en caso de que el autor se deje en blanco (el 
+usuario introduzca una cadena vacía). Emplea un "ToString" adecuado para 
+mostrar los resultados de las búsquedas, que devuelva todos los datos como 
+parte de una misma cadena, separados por " - ".*/
 
 using System;
+using System.Configuration;
 
-public class Libro 
+public class Libro
 {
 
     protected string titulo;
     protected string autor;
     protected short anyoPubli;
 
-    public Libro( string nuevoTitulo, string nuevoAutor, int nuevoAnyoPubli ) 
+    public Libro(string titulo, string autor, int anyoPubli)
     {
-        titulo = nuevoTitulo;
-        autor = nuevoAutor;
-        anyoPubli = ( short) nuevoAnyoPubli;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anyoPubli = (short) anyoPubli;
+    }
+
+    public Libro(string titulo, int anyoPubli)
+        : this(titulo, "Desconocido", anyoPubli)
+    {
     }
 
     public string GetTitulo() { return titulo; }
     public string GetAutor() { return autor; }
     public int GetAnyoPubli() { return anyoPubli; }
 
-    public void SetTitulo( string nuevoTitulo ) 
-    { 
-        titulo = nuevoTitulo; 
+    public void SetTitulo(string nuevoTitulo)
+    {
+        titulo = nuevoTitulo;
     }
-    
-    public void SetAutor( string nuevoAutor ) 
+
+    public void SetAutor(string nuevoAutor)
     {
         autor = nuevoAutor;
     }
 
-    public void SetAnyoPubli( int nuevoAnyoPubli ) 
-    { 
-        anyoPubli = (short)nuevoAnyoPubli; 
+    public void SetAnyoPubli(int nuevoAnyoPubli)
+    {
+        anyoPubli = (short)nuevoAnyoPubli;
     }
 
-    public void Mostrar() 
+    public void Mostrar()
     {
-        Console.WriteLine( "Título : " + titulo );
-        Console.WriteLine( "Autor: " + autor );
-        Console.WriteLine( "Año de publicación: " + anyoPubli );
+        Console.WriteLine("Título : " + titulo);
+        Console.WriteLine("Autor: " + autor);
+        Console.WriteLine("Año de publicación: " + anyoPubli);
     }
 
-    public bool Contiene( string texto) 
+    public bool Contiene(string texto)
     {
-        return titulo.ToUpper().Contains( texto.ToUpper() ) || 
-            autor.ToUpper().Contains( texto.ToUpper() );
+        return titulo.ToUpper().Contains(texto.ToUpper()) ||
+            autor.ToUpper().Contains(texto.ToUpper());
+    }
+
+    public override string ToString()
+    {
+        return titulo + " - " + autor + " - " + anyoPubli;
     }
 }
 
@@ -69,72 +82,76 @@ class GestorLibros
         Console.WriteLine();
     }
 
-    static void Main() 
+    static void Main()
     {
         const int CAPACIDADMAXIMA = 50000;
-        Libro [] libros = new Libro [CAPACIDADMAXIMA];
-        string opcion="";
+        Libro[] libros = new Libro[CAPACIDADMAXIMA];
+        string opcion = "";
         string tituloAux, autorAux, textoBuscar;
         short anyoAux;
         Libro libroAux;
         int contadorLibros = 0;
-        
-        while(opcion != "S")
+
+        while (opcion != "S")
         {
             MostrarMenu();
             opcion = Console.ReadLine().ToUpper();
-            
-            switch(opcion)
+
+            switch (opcion)
             {
                 //Añadir un libro nuevo
-                case "1": 
+                case "1":
                     Console.Write("Introduzca el título: ");
                     tituloAux = Console.ReadLine();
                     Console.Write("Introduzca el autor: ");
                     autorAux = Console.ReadLine();
                     Console.Write("Introduzca el año de publicación: ");
                     anyoAux = Convert.ToInt16(Console.ReadLine());
+
+                    if (autorAux == "")
+                        libroAux = new Libro(tituloAux, anyoAux);
+                    else
+                        libroAux = new Libro(tituloAux, autorAux, anyoAux);
                     
-                    libroAux = new Libro(tituloAux, autorAux, anyoAux);
                     libros[contadorLibros] = libroAux;
-                    
                     contadorLibros++;
                     break;
-                    
+
                 //Mostrar todos los libros
-                case "2": 
-                    for( int i = 0; i < contadorLibros; i++)
+                case "2":
+                    for (int i = 0; i < contadorLibros; i++)
                     {
-                        Console.WriteLine("LIBRO "+(i+1)+":");
+                        Console.WriteLine("LIBRO " + (i + 1) + ":");
                         libros[i].Mostrar();
                     }
                     break;
-                    
+
                 //Buscar libros que contengan un texto determinado
-                case "3": 
+                case "3":
                     Console.Write("Introduce el texto a buscar: ");
                     textoBuscar = Console.ReadLine();
                     bool encontrado = false;
-                    for(int i = 0; i < contadorLibros; i++)
+                    for (int i = 0; i < contadorLibros; i++)
                     {
-                        if(libros[i].Contiene(textoBuscar))
+                        if (libros[i].Contiene(textoBuscar))
                         {
-                            libros[i].Mostrar();
+                            Console.WriteLine(libros[i]);
                             encontrado = true;
                         }
                     }
-                    if (! encontrado)
+                    if (!encontrado)
                     {
                         Console.WriteLine("No encontrado");
                     }
                     break;
-                    
+
                 // Salir
                 case "S":
                     Console.WriteLine("Hasta pronto!");
                     break;
 
-                default: Console.WriteLine("Opción NO válida.");
+                default:
+                    Console.WriteLine("Opción NO válida.");
                     break;
             }
         }
