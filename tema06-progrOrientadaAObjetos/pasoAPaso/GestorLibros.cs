41c8829154d9adb003c2a9450aa5f7c3e20720de
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-02-01 13:22:42 +0100

    Gestor de libros, versión 1 (ej.148)

--- tema06-progrOrientadaAObjetos/pasoAPaso/GestorLibros.cs
+++ tema06-progrOrientadaAObjetos/pasoAPaso/GestorLibros.cs
@@ -1,30 +1,22 @@
-﻿/* Nombre: Andrés (...)
- * Curso: 1º DAM. Semipresencial 2024-2025 */
-
-/* Ejercicio 141. Biblioteca con getters y setters.
+/* MARTA (...), retoques menores por Nacho
  * 
- * De cara a irnos acercando a una versión orientada a objetos del proyecto
- * de la Biblioteca (ejercicio 113), crea una clase Libro, con atributos:
- * titulo (string), autor (string), anyoPubli (entero corto). 
- * 
- * Debes preparar setters y getters para todos ellos, pero los getters
- * numéricos devolverán datos "int" (en vez de "short"), y los setters
- * numéricos recibirán datos "int". 
- * 
- * Crea también un constructor adecuado, un método Mostrar (void) que muestre
- * los datos de un Libro en pantalla, y un método Contiene (booleano) que
- * devuelva "true" si el título o el autor contienen el texto que se indique
- * como parámetro. */
+148. A partir de la clase "Libro" (ejercicio 142, en su "solución 
+oficial"), crea un programa que permita tener hasta 25000 libros, y un 
+menú con el que se pueda: Añadir un libro nuevo (opción "1"), Mostrar 
+todos los libros (opción "2"), Buscar libros que contengan un texto 
+determinado (opción "3"), Salir (opción "S"). */
 
 using System;
 
-class Libro {
+public class Libro 
+{
 
     protected string titulo;
     protected string autor;
     protected short anyoPubli;
 
-    public Libro( string nuevoTitulo, string nuevoAutor, int nuevoAnyoPubli ) {
+    public Libro( string nuevoTitulo, string nuevoAutor, int nuevoAnyoPubli ) 
+    {
         titulo = nuevoTitulo;
         autor = nuevoAutor;
         anyoPubli = ( short) nuevoAnyoPubli;
@@ -34,43 +26,117 @@ class Libro {
     public string GetAutor() { return autor; }
     public int GetAnyoPubli() { return anyoPubli; }
 
-    public void SetTitulo( string nuevoTitulo ) { 
+    public void SetTitulo( string nuevoTitulo ) 
+    { 
         titulo = nuevoTitulo; 
     }
     
-    public void SetAutor( string nuevoAutor ) {
+    public void SetAutor( string nuevoAutor ) 
+    {
         autor = nuevoAutor;
     }
 
-    public void SetAnyoPubli( int nuevoAnyoPubli ) { 
+    public void SetAnyoPubli( int nuevoAnyoPubli ) 
+    { 
         anyoPubli = (short)nuevoAnyoPubli; 
     }
 
-    public void Mostrar() {
+    public void Mostrar() 
+    {
         Console.WriteLine( "Título : " + titulo );
         Console.WriteLine( "Autor: " + autor );
         Console.WriteLine( "Año de publicación: " + anyoPubli );
     }
 
-    public bool Contiene( string texto) {
+    public bool Contiene( string texto) 
+    {
         return titulo.ToUpper().Contains( texto.ToUpper() ) || 
             autor.ToUpper().Contains( texto.ToUpper() );
     }
-
 }
 
-class PruebaLibro {
+class GestorLibros
+{
+    public static void MostrarMenu()
+    {
+        Console.WriteLine();
+        Console.WriteLine("------ MENÚ -------");
+        Console.WriteLine("1. Añadir un libro nuevo");
+        Console.WriteLine("2. Mostrar todos los libros");
+        Console.WriteLine("3. Buscar libros que contengan un texto determinado");
+        Console.WriteLine("S. Salir");
+        Console.WriteLine("-------------------");
+        Console.WriteLine();
+    }
 
-    static void Main() {
+    static void Main() 
+    {
+        const int CAPACIDADMAXIMA = 50000;
+        Libro [] libros = new Libro [CAPACIDADMAXIMA];
+        string opcion="";
+        string tituloAux, autorAux, textoBuscar;
+        short anyoAux;
+        Libro libroAux;
+        int contadorLibros = 0;
+        
+        while(opcion != "S")
+        {
+            MostrarMenu();
+            opcion = Console.ReadLine().ToUpper();
+            
+            switch(opcion)
+            {
+                //Añadir un libro nuevo
+                case "1": 
+                    Console.Write("Introduzca el título: ");
+                    tituloAux = Console.ReadLine();
+                    Console.Write("Introduzca el autor: ");
+                    autorAux = Console.ReadLine();
+                    Console.Write("Introduzca el año de publicación: ");
+                    anyoAux = Convert.ToInt16(Console.ReadLine());
+                    
+                    libroAux = new Libro(tituloAux, autorAux, anyoAux);
+                    libros[contadorLibros] = libroAux;
+                    
+                    contadorLibros++;
+                    break;
+                    
+                //Mostrar todos los libros
+                case "2": 
+                    for( int i = 0; i < contadorLibros; i++)
+                    {
+                        Console.WriteLine("LIBRO "+(i+1)+":");
+                        libros[i].Mostrar();
+                    }
+                    break;
+                    
+                //Buscar libros que contengan un texto determinado
+                case "3": 
+                    Console.Write("Introduce el texto a buscar: ");
+                    textoBuscar = Console.ReadLine();
+                    bool encontrado = false;
+                    for(int i = 0; i < contadorLibros; i++)
+                    {
+                        if(libros[i].Contiene(textoBuscar))
+                        {
+                            libros[i].Mostrar();
+                            encontrado = true;
+                        }
+                    }
+                    if (! encontrado)
+                    {
+                        Console.WriteLine("No encontrado");
+                    }
+                    break;
+                    
+                // Salir
+                case "S":
+                    Console.WriteLine("Hasta pronto!");
+                    break;
 
-        Libro l = new Libro( "Libro de prueba.", "Autor de prueba.", 2022 );
-        l.SetAnyoPubli( 2025 );
-        l.Mostrar();
-
-        if ( l.Contiene( "mm" ) ) {
-            Console.WriteLine( "El texto \"mm\" ha sido encontrado." );
-        } else {
-            Console.WriteLine( "El texto \"mm\" no ha sido encontrado." );
+                default: Console.WriteLine("Opción NO válida.");
+                    break;
+            }
         }
     }
 }
